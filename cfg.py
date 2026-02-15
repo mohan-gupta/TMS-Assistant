733fa2efb4e98883c526f01894f5525e84d7245c
Mohan Gupta <mohankrishnagupta@gmail.com>
2026-02-16 00:06:01 +0530

    fixed typo and updated cfg.py

--- cfg.py
+++ cfg.py
@@ -4,6 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Check directories
+directory_path = "./upload_dir/"
+
+if not os.path.exists(directory_path):
+    os.mkdir("./upload_dir/")
+
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 qdrant_api_key = os.getenv("QDRANT_API_KEY")
 qdrant_cluster_url = os.getenv("QDRANT_CLUSTER_URL")
